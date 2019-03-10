7e005939b4756507bc85d965c46d3a6209e16ba9
pgonzalez003 <pgonzalez003@gmail.com>
2019-03-09 23:58:43 -0800

    push

---
+++ assets/javascript/app.js
@@ -0,0 +1,92 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC9xpYVXaoIZfBmjA6T5o7xzTv_cPQdVS4",
+    authDomain: "trainhomework-12f0c.firebaseapp.com",
+    databaseURL: "https://trainhomework-12f0c.firebaseio.com",
+    projectId: "trainhomework-12f0c",
+    storageBucket: "trainhomework-12f0c.appspot.com",
+    messagingSenderId: "203503065462"
+  };
+  firebase.initializeApp(config);
+
+    var database = firebase.database();
+    
+    // Initial values
+
+    var trainName = "";
+    var destination = "";
+    var frequency = "";
+    var nextArrival = "";
+    var minutesAway = "";
+
+    // On submit button press
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+
+        // Store form data into variables
+        trainName = $("#trainName").val();
+        destination = $("#destination").val();
+        frequency = $("#frequency").val();
+        nextArrival = $("#firstTrainTime").val();
+        minutesAway = $("#minutesAway").val();
+       
+
+        // Push data into database as object
+        database.ref().push({
+
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            nextArrival: nextArrival,
+            minutesAway; minutesAway,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        console.log("click");
+    }); //END OF SUBMIT BUTTON PRESS
+
+    // On Child Added to Database
+    dataRef.ref().on("child_added", function (childSnapshot) {
+
+        // Store database object data into variables
+       console.log(childSnapshot.val().trainName);
+       console.log(childSnapshot.val().destination);
+       console.log(childSnapshot.val().frequency);
+       console.log(childSnapshot.val().nextArrival);
+       console.log(childSnapshot.val().minutesAway);
+
+
+
+       $("#infoTable".append("<tr></tr>").append(
+        "<td>" + trainName + "</td>",
+        "<td>" + destination + "</td>",
+        "<td>" + Frequency + "</td>",
+        "<td>" + nextTrainTime + "</td>",
+        "<td>" + minUntilNext + "</td>"
+       ))
+        // Train Time logic
+        var currentTime = moment();
+        var firstTimeConverted = moment(newFirstTrainTime, "hh:mm").subtract(1, "years");
+        var timeDiff = moment().diff(moment(firstTimeConverted), "minutes")
+        var timeRemainder = timeDiff % newFrequency
+        var minUntilNext = newFrequency - timeRemainder
+        var nextTrainTime = moment().add(minUntilNext, "minutes").format("hh:mm")
+
+        // Console log time logic
+        // console.log(currentTime.format("hh:mm"))
+        // console.log(firstTimeConverted)
+        // console.log(timeDiff)
+        // console.log(timeRemainder)
+        // console.log(minUntilNext)
+        // console.log(nextTrainTime)
+
+        // Append new data into display table
+        $("#infoTable").append("<tr></tr>").append(
+            "<td>" + newTrainName + "</td>",
+            "<td>" + newDestination + "</td>",
+            "<td>" + newFrequency + "</td>",
+            "<td>" + nextTrainTime + "</td>",
+            "<td>" + minUntilNext + "</td>")
+
+    }) //END OF ON CHILD ADDED EVENT
+
+}) //END OF DOCUMENT READ
